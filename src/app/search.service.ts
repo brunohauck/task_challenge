b0697b46803e42f760d72aa5e67d8c38919362e9
Giovanni Silva <giovanni@atende.info>
2018-05-03 20:20:50 -0300

    refact autocomplete

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -5,11 +5,12 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/switchMap';
+import { Employee } from './domain/employee/employee';
 
 
 @Injectable()
 export class SearchService {
-    
+
   baseUrl: string = 'http://marmita.idsgeo.com/index.php/employee/get_like';
   queryUrl: string = '/';
   //baseUrl: string = 'https://api.cdnjs.com/libraries';
@@ -17,22 +18,12 @@ export class SearchService {
 
   constructor(private http: Http) { }
 
-  search(terms: Observable<string>) {
-    return terms.debounceTime(400)
-      .distinctUntilChanged()
-      .switchMap(term => this.searchEntries(term));
-  }
-
-  searchEntries(term) {
-    if(term == ""){
+  searchEntries(term: string): Observable<any> {
+    if (term == "") {
       term = "00000000000";
-      return this.http
+    }
+    return this.http
       .get(this.baseUrl + this.queryUrl + term)
       .map(res => res.json());
-    }else{
-    return this.http
-        .get(this.baseUrl + this.queryUrl + term)
-        .map(res => res.json());
-    }    
   }
 }
